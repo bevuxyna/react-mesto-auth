e715057d52b7bb0d60944265ecfe0f03344d05d0
bevuxyna <wannhorn@gmail.com>
2022-06-22 21:29:07 +0300

    feature: login form

---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+
+function Login(props) {
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (evt) => {
+        const {name, value} = evt.target;
+        setUserData({
+            ...userData, [name]: value
+        });
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        props.onLogin(userData.password, userData.email);
+        setUserData({ email: '', password: '' });
+    }
+
+    return(
+        <div className="form__container">
+            <div className="form__block">
+                <h2 className="form__title">Вход</h2>
+                <form className="form" onSubmit={handleSubmit}>
+                    <input
+                        className="form__input"
+                        name="email"
+                        type="email"
+                        id="email"
+                        value={userData.email}
+                        onChange={handleChange}
+                        placeholder="Email"
+                        required
+                    />
+
+                    <input
+                        className="form__input"
+                        name="password"
+                        type="password"
+                        id="password"
+                        value={userData.password}
+                        onChange={handleChange}
+                        placeholder="Пароль"
+                        required
+                    />
+
+                    <button
+                        className="form__button-submit form__button-submit_type_login"
+                        type="submit"
+                    >
+                        Войти
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
